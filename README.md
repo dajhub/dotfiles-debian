d0bcebc723d9e52f58caa37684e07329c73f7e51
dajhub <dajhub63@gmail.com>
2022-04-21 21:53:54 +0100

    added application shell script and updated readme file

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # dotfiles-debian
-Setup for a debian install
+Setup for a debian install (unstable version)
+
+##Installation of Applications
+===
+The applications can be found in this script: 'install-apps.sh'.  To install the applications using the script...
+
+~~~
+sudo ./install-apps.sh
+~~~
+
+Within the applications being installed, one of them is the 'xfce4-whiskermenu-plugin'.  Once placed on the panel, if you want to launch it with the 'window' button you need to open the keyboard settings, select 'application shortcuts' and create a new shortcut with the following command: 'xfce4-popup-whiskermenu'.
+
---
+++ install-apps.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+  echo "Please run as root"
+  exit 1
+fi
+
+sudo apt-get update
+sudo apt-get upgrade
+############################################################
+### These installs are from the debian unstable repositories
+############################################################
+
+### Programs
+sudo apt install -y geany
+sudo apt install -y pinta
+sudo apt install -y gcolor3
+sudo apt install -y abiword
+sudo apt install -y flameshot
+sudo apt install -y viewnior
+sudo apt install -y gnome-boxes
+sudo apt install -y kitty
+
+### Other
+sudo apt install -y git
+sudo apt install -y curl
+
+### xfce
+sudo apt install -y xfce4-whiskermenu-plugin
+
+### Librewolf -- debian unstable version [source: https://librewolf.net/installation/debian/]
+echo 'deb http://download.opensuse.org/repositories/home:/bgstack15:/aftermozilla/Debian_Unstable/ /' | sudo tee /etc/apt/sources.list.d/home:bgstack15:aftermozilla.list
+curl -fsSL https://download.opensuse.org/repositories/home:bgstack15:aftermozilla/Debian_Unstable/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_bgstack15_aftermozilla.gpg > /dev/null
+sudo apt update
+sudo apt install librewolf -y
+
+echo "################################################################"
+echo "#################### Software installed  #######################"
+echo "################################################################"
+
+
+
+
+
+
+
+
